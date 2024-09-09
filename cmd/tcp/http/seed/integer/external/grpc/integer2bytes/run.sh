826a9fef2c4c45fe09d204e8af8052fe99aada2b
yanagitani <takanori.yanagitani@gmail.com>
2024-09-10 07:41:11 +0900

    docs: sample benchmark result

--- cmd/tcp/http/seed/integer/external/grpc/integer2bytes/run.sh
+++ cmd/tcp/http/seed/integer/external/grpc/integer2bytes/run.sh
@@ -3,6 +3,6 @@
 export ENV_SEED_GRPC_ADDR=localhost:50051
 export ENV_TARGET_URL=http://localhost:9088/
 export ENV_POST_BODY_CONTENT_TYPE=application/octet-stream
-export ENV_LOOP_MAX=131072
+export ENV_LOOP_MAX=1048576
 
 time ./integer2bytes
